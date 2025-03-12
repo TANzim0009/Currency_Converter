72ac2845810152a4f75950dfa0f8e990c9a788b4
TANzim0009 <140909012+TANzim0009@users.noreply.github.com>
2025-03-12 13:45:53 +0600

    Update CurrencyConverter.java

--- CurrencyConverter.java
+++ CurrencyConverter.java
@@ -3,16 +3,24 @@ import java.awt.*;
 public class CurrencyConverter {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Currency Converter");
-        frame.setSize(300, 200);
-        frame.setLayout(new GridLayout(2,2,5,5));
+        frame.setSize(700, 500);
+        frame.setLayout(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JTextField bdtField = new JTextField(10);
-        JTextField dollarField = new JTextField(10);
+        JLabel label1 = new JLabel("BDT:");
+        label1.setBounds(5, 10, 50, 20);
+        frame.add(label1);
 
-        frame.add(new JLabel("BDT:"));
+        JTextField bdtField = new JTextField();
+        bdtField.setBounds(85, 10, 150, 20);
         frame.add(bdtField);
-        frame.add(new JLabel("Dollars:"));
+
+        JLabel label2 = new JLabel("DOLLAR:");
+        label2.setBounds(5, 35, 80, 20);
+        frame.add(label2);
+
+        JTextField dollarField = new JTextField();
+        dollarField.setBounds(85, 35, 150, 20);
         frame.add(dollarField);
         
         frame.setVisible(true);
