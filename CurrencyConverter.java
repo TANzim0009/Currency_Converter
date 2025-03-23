1fec7d71a3bd8c32163f212cf79eadf565b23f88
TANzim0009 <140909012+TANzim0009@users.noreply.github.com>
2025-03-23 20:35:39 +0600

    Update CurrencyConverter.java

--- CurrencyConverter.java
+++ CurrencyConverter.java
@@ -1,5 +1,8 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class CurrencyConverter {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Currency Converter");
@@ -22,7 +25,43 @@ public class CurrencyConverter {
         JTextField dollarField = new JTextField();
         dollarField.setBounds(85, 35, 150, 20);
         frame.add(dollarField);
-        
+
+        JRadioButton bdtToDollar = new JRadioButton("BDT to Dollar", true);
+        bdtToDollar.setBounds(20, 60, 120, 25);
+        frame.add(bdtToDollar);
+
+        JRadioButton dollarToBdt = new JRadioButton("Dollar to BDT");
+        dollarToBdt.setBounds(150, 60, 120, 25);
+        frame.add(dollarToBdt);
+
+        ButtonGroup conversionGroup = new ButtonGroup();
+        conversionGroup.add(bdtToDollar);
+        conversionGroup.add(dollarToBdt);
+
+        JButton convert = new JButton("Convert");
+        convert.setBounds(100, 90, 100, 30);
+        frame.add(convert);
+
+        convert.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    if (bdtToDollar.isSelected()) {
+                        double bdt = Double.parseDouble(bdtField.getText());
+                        double dollars = bdt / 124.0; // Assuming 1 USD = 110 BDT
+                        dollarField.setText(String.format("%.2f", dollars));
+                    } else if (dollarToBdt.isSelected()) {
+                        double dollars = Double.parseDouble(dollarField.getText());
+                        double bdt = dollars * 124.0;
+                        bdtField.setText(String.format("%.2f", bdt));
+                    }
+                } catch (NumberFormatException ex) {
+                    JOptionPane.showMessageDialog(frame, "Invalid input! Enter a valid number.");
+                }
+            }
+        });
+
+
         frame.setVisible(true);
     }
 }
